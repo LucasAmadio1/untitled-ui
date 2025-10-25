223a6f5317e3ba821fa000a099589067df23bc5e
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-10-25 14:58:35 -0300

    bio component

---
+++ src/components/Textarea.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+
+interface TextareaProps extends ComponentProps<'textarea'> {}
+
+export function Textarea({ ...props }: TextareaProps) {
+  return (
+    <textarea
+      {...props}
+      className="min-h-32 w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+    />
+  )
+}
